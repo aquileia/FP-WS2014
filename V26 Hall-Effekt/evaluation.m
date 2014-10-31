a845c7e883fd0bf4f6598e47d1f005bdc6ae9c94
aquileia <sk.aquileia@gmail.com>
2014-10-31 12:27:14 +0100

    Move temperature vector into the script
    As we won't reuse the script, we may as well list the temperatures directly.

--- V26 Hall-Effekt/evaluation.m
+++ V26 Hall-Effekt/evaluation.m
@@ -4,7 +4,9 @@ d = 0.31e-4   % sample thickness in cm
 f_a=1;        % correction factor, approximately 1
 
 % vector of temperatures, also used for accessing data files
-T=[82];
+T=[                                                                                 82,  85,  87,  90,  93,  95,  98, 
+   103, 105, 110, 116, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185,      191,      195,
+   200, 205, 210, 215, 220, 225, 230, 235, 242, 245, 250, 255, 261, 265, 270, 275, 280, 285,      290, 291, 295, 297, 300];
 
 for i=1:length(T)
     data=dlmread(['T',num2str(T(i)),'_both.txt'],'\t');
