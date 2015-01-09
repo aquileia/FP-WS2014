c7b9d7633b9d6f0d269170eec8e89d82e45278d0
aquileia <sk.aquileia@gmail.com>
2015-01-09 19:06:21 +0100

    Import fit parametres, prepare for scaling

--- V39_Laser-Spektroskopie/Matlab/V39.m
+++ V39_Laser-Spektroskopie/Matlab/V39.m
@@ -1,46 +1,48 @@
 %% Process absorption spectra 
 function [ ] = V39( )
-global gof x y
+global gof spec model x y
 
-i = 0;
+model = 'gauss2';
+i = 16;
 % for i=0:16
     data = dlmread(['TEK' sprintf('%04d', i) '.CSV'], ',', 'D1..E2500');
     rep = find(data(:,2) ~= data(1,2),1);
     x = 1000*data(rep:end,1);
     y = data(rep:end,2);
     % normalize intensity (correct slope)
-    if ismember(i,[0,1,10])
-        [rough, gof] = fit(x,y,'poly2','Robust','on');
+    %  if ismember(i,[0,1,10])
+        [rough, gof] = fit(x,y,model,'Robust','on');
         fine = refine_fit(rough);
         ultra = refine_fit(fine);
-      % plot(fine,'r-',x,y,'k.',spec,'g.')
+        plot(ultra,'r-',x,y,'k.',spec,'g.')
         y = y ./ feval(ultra,x);
-    end;
+    % end;
 % end;
 
-P1 = fit(x,1-y,'gauss1', 'Exclude', x<-15 | x> -9);
-P2 = fit(x,1-y,'gauss1', 'Exclude', x< -5 | x>  2);
-P3 = fit(x,1-y,'gauss1', 'Exclude', x<  7 | x> 13);
-P4 = fit(x,1-y,'gauss1', 'Exclude', x< 13 | x> 18);
-
-plot(P1,x,1-y, 'k.')
-hold on
-plot(P2)
-plot(P3)
-plot(P4)
-hold off
-
-legend('data', legend_fit(P1), legend_fit(P2), legend_fit(P3), legend_fit(P4))
+% P1 = fit(x,1-y,'gauss1', 'Exclude', x<-15 | x> -9);
+% P2 = fit(x,1-y,'gauss1', 'Exclude', x< -5 | x>  2);
+% P3 = fit(x,1-y,'gauss1', 'Exclude', x<  7 | x> 13);
+% P4 = fit(x,1-y,'gauss1', 'Exclude', x< 13 | x> 18);
+% 
+% plot(P1,x,1-y, 'k.')
+% hold on
+% plot(P2)
+% plot(P3)
+% plot(P4)
+% hold off
+% 
+% legend('data', legend_fit(P1), legend_fit(P2), ...
+%                legend_fit(P3), legend_fit(P4), 'Location','NorthWest')
 end
 
 
 %% Use a fit to discard outliers and fit again
 function [new_fit] = refine_fit(old_fit)
-global gof x y
+global gof  spec model x y
 
     ind = abs (y - feval(old_fit,x)) > gof.rmse;
     spec = excludedata(x,y,'indices',ind);
-    [new_fit, gof] = fit(x,y,'poly2','Robust','on','Exclude',spec);
+    [new_fit, gof] = fit(x,y,model,'Robust','on','Exclude',spec);
 end
 
 %% Format a tolerance interval in physical notation
@@ -59,5 +61,6 @@ function [entry] = legend_fit(fit)
     h = [coeffvalues(fit); confint(fit)];
     c = h(1,:);
     dc = max(h(3,:)-c, c-h(2,:));
-    entry = ['µ = ' format_tol(c(2), dc(2)) ' , \sigma = ' format_tol(c(3), dc(3))];
+    entry = ['µ = ' format_tol(c(2), dc(2)) ' , ' ...
+             '\sigma = ' format_tol(c(3)/sqrt(2), dc(3)/sqrt(2))];
 end
