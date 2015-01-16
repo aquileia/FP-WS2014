5c2681110f3ef9f8cc0c18b0566d5dd086bd88f3
aquileia <sk.aquileia@gmail.com>
2015-01-16 10:44:58 +0100

    V39: Scaling, dips for Peak 1

--- V39_Laser-Spektroskopie/Matlab/V39.m
+++ V39_Laser-Spektroskopie/Matlab/V39.m
@@ -6,7 +6,7 @@ model = 'poly2';
 
 data = dlmread(['TEK' sprintf('%04d', index) '.CSV'], ',', 'D1..E2500');
 rep = find(data(:,2) ~= data(1,2),1);
-x = 1000*data(rep:end,1);
+x = 3.774-255.55*data(rep:end,1);
 y = data(rep:end,2);
 % normalize intensity (correct slope)
 if (lines > 1)
@@ -15,27 +15,27 @@ if (lines > 1)
     ultra = refine_fit(fine);
     plot(ultra,'r-',x,y,'k.',spec,'g.')
     y = y ./ feval(ultra,x);
-else
+elseif  (lines == 1)
     lin = fit( x([1 end]), y([1 end]), 'poly1');
     y = y ./ feval(lin,x);
     plot(x,y)
 end
 
 
-P1 = fit(x,1-y,'gauss1', 'Exclude', x<-15 | x> -9);
-P2 = fit(x,1-y,'gauss1', 'Exclude', x< -5 | x>  2);
-P3 = fit(x,1-y,'gauss1', 'Exclude', x<  7 | x> 13);
-P4 = fit(x,1-y,'gauss1', 'Exclude', x< 13 | x> 18);
-
-plot(P1,x,1-y, 'k.')
-hold on
-plot(P2)
-plot(P3)
-plot(P4)
-hold off
-
-legend('data', legend_fit(P1), legend_fit(P2), ...
-               legend_fit(P3), legend_fit(P4), 'Location','NorthWest')
+% P1 = fit(x,1-y,'gauss1', 'Exclude', x<-15 | x> -9);
+% P2 = fit(x,1-y,'gauss1', 'Exclude', x< -5 | x>  2);
+% P3 = fit(x,1-y,'gauss1', 'Exclude', x<  7 | x> 13);
+% P4 = fit(x,1-y,'gauss1', 'Exclude', x< 13 | x> 18);
+% 
+% plot(P1,x,1-y, 'k.')
+% hold on
+% plot(P2)
+% plot(P3)
+% plot(P4)
+% hold off
+% 
+% legend('data', legend_fit(P1), legend_fit(P2), ...
+%                legend_fit(P3), legend_fit(P4), 'Location','NorthWest')
 end
 
 
