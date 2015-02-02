dbdfdac3ad7b0452afc03e9d24a7af52465f5f96
aquileia <sk.aquileia@gmail.com>
2015-02-02 16:43:56 +0100

    Fix format_tol for dc > 30
    sprintf doesn't accept negative decimals

--- V30_Nonlinear_Optics/matlab/format_tol.m
+++ V30_Nonlinear_Optics/matlab/format_tol.m
@@ -2,7 +2,7 @@ function [coeff] = format_tol(c, dc)
     %% Format a tolerance interval in physical notation
     n = floor(log10(dc/3));
     dc_r = ceil(dc*10^-n)*10^n;
-    % c_r = round(dc*10^-n)*10^n;
-    format = ['%0.' num2str(-n) 'f'];
-    coeff = [sprintf(format,c) ' \pm ' sprintf(format,dc_r) ];
+    c_r = round(c*10^-n)*10^n;
+    format = ['%0.' int2str(max(-n,0)) 'f'];
+    coeff = [sprintf(format,c_r) ' \pm ' sprintf(format,dc_r) ];
 end
